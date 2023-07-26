4f08b6226902417d90f40cfad8eab4d69fb47d2b
yani pranay <33549205+yanipanay@users.noreply.github.com>
2023-07-26 22:55:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function decimalToBinary(num) {
   // Write your code here
+	let ans = '';
+	while(num>0){
+		if((num&1)>0) ans = "1"+ans;
+		else ans = '0'+ans;
+
+		num= (num>>1)
+	}
+
+	return ans;
 }
 
 window.decimalToBinary = decimalToBinary;
